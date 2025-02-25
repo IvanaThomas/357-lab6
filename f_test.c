e639bd5e79643e8d931807bd6c66810bfa45d06b
Ivana <ivana_th@icloud.com>
2025-02-25 10:09:27 -0800

    f_test.c progress

--- f_test.c
+++ f_test.c
@@ -2,13 +2,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char argv[]){
+void limit_fork(rlim_t max_procs){
+	struct rlimit rl;
+	if (getrlimit(RLIMIT_NPROC, &rl)){
+		perror("getrlimit");
+		exit(-1);
+	}
+
+	rl.rlim_cur = max_procs;
+
+	if (setrlimit(RLIMIT_NPROC, &rl)){
+		perror("setrlimit");
+		exit(-1);
+	}
+}
+int main(int argc, char* argv[]){
+	limit_fork(50);
 	if (argc < 2) {
 		printf("Error: Not enough args provided");
 		exit(1);
 	}
 
-	int N = argv[1];
+	int N = (int)argv[1];
 	
 	//fork
 	pid_t pid = fork();
@@ -19,5 +34,18 @@ int main(int argc, char argv[]){
 	}
 
 	//child process
+	else if (pid == 0){
+		for (int i = 1; i <= N; i +=2){
+			printf("%d\n", i);
+		}
+		exit(1);
+	}
+
+	else {
+		for (int i = 2; i <= N; i += 2){
+			printf("\t%d\n", i);
+		}
+		wait(NULL);
+	}
 	
 }
