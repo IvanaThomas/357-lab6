b22e85693e0bcab210f0297640863849c3883c77
IvanaThomas <ivana_th@icloud.com>
2025-02-26 17:44:29 -0800

    odds and evens

---
+++ evens.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]){
+
+        if (argc < 2) {
+                printf("Error: Not enough args provided");
+                exit(1);
+        }
+
+        int N;
+        sscanf(argv[1], "%d", &N);
+
+	for (int i = 2; i <= N; i+=2){
+                printf("%d\n", i);
+        }
+}
