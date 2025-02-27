a128e3f0543def2c1cb1e2b9203d1d1f37cb267d
Ivana <ivana_th@icloud.com>
2025-02-27 09:46:50 -0800

    fe_test

---
+++ fe_test.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+
+void limit_fork(rlim_t max_procs){
+	struct rlimit rl;
+	if (getrlimit(RLIMIT_NPROC, &rl)){
+		perror("getrlimit");
+		exit(-1);
+	}
+
+	rl.rlim_cur = max_procs;
+
+	if (setrlimit(RLIMIT_NPROC, &rl)){
+		perror("setrlimit");
+		exit(-1);
+	}
+}
+
+int main(int argc, char* argv[]){
+	limit_fork(50);
+	if (argc < 2) {
+		printf("Error: Not enough args provided");
+		exit(1);
+	}
+
+	int N;
+	sscanf(argv[1], "%d", &N);
+	
+	//fork
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		perror("Error creating  fork");
+		exit(1);
+	}
+
+	//child process
+	else if (pid == 0){
+		execl("odds.c", NULL);
+	}
+
+	else {
+		execl("evens.c", NULL);
+	}
+	
+}
